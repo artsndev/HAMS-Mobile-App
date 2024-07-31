e62296f1fe4eb80d92c52935953dd501e8ecc1cb
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-08-01 00:08:08 +0800

    coded by artisandev

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>This is Login Page</h1>
+</template>
--- src/components/layouts/AppBar.vue
+++ src/components/layouts/AppBar.vue
@@ -1,50 +1,45 @@
+<!-- eslint-disable vue/no-v-text-v-html-on-component -->
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-app-bar flat>
-    <v-container class="px-4">
-            <v-row no-gutters>
-                <v-col cols="5" xxl="4" xl="4" lg="8">
-                  <v-avatar size="35">
-                    <v-icon size="50">mdi-account</v-icon>
-                      <!-- <img src="@/assets/avatar.png" alt="Avatar" > -->
-                      <v-dialog v-model="dialog" persistent activator="parent" width="500px" >
-                        <v-card style="border-radius: 30px; background-color: #EEEEEE;" >
-                          <v-toolbar density="compact">
-                            <v-toolbar-title class="text-center font-weight-medium ms-10" style="font-size: medium;">{{ email }}</v-toolbar-title>
-                            <v-btn icon="mdi-close" dark  @click="dialog = false" density="compact"></v-btn>
-                          </v-toolbar>
-                          <v-avatar size="100" class="mx-auto">
-                            <v-icon size="130" class="rounded-circle">mdi-account</v-icon>
-                            <!-- <img src="@/assets/avatar.png" alt="Avatar"> -->
-                          </v-avatar>
-                          <h2 class="mx-auto font-weight-regular">Hi, {{ name }}!</h2>
-                          <v-btn class="mb-4 mx-auto" rounded color="primary" variant="outlined">Manage your Account</v-btn>
-                          <v-container>
-                            <v-card style="border-radius: 25px;">
-                              <v-list>
-                                <v-list-item prepend-avatar="@/assets/avatar.png" :title="name" :subtitle="email">
-                                </v-list-item>
-                              </v-list>
-                              <v-divider></v-divider>
-                              <v-list :lines="false" nav>
-                                <v-list-item v-for="(item, i) in items" :key="i" :value="item" rounded="xl" @click="handleItemClick(item)">
-                                  <template v-slot:prepend>
-                                    <v-icon :icon="item.icon"></v-icon>
-                                  </template>
-                                  <v-list-item-title v-text="item.text" style="font-size: medium;"></v-list-item-title>
-                                </v-list-item>
-                              </v-list>
-                            </v-card>
-                          </v-container>
-                        </v-card>
-                      </v-dialog>
-                  </v-avatar>
-                </v-col>
-                <v-col cols="5">
-                  <v-img src="@/assets/logo.png" alt="Logo" max-width="70" max-height="70"></v-img>
-                </v-col>
-            </v-row>
-        </v-container>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <template v-slot:append>
+      <v-avatar size="45" class="me-2">
+        <img src="@/assets/images/avatar.jpg" alt="Avatar" style="object-fit: cover; width: 100%; height: 100%;">
+        <v-dialog v-model="dialog" persistent activator="parent" width="500px" >
+          <v-card style="border-radius: 30px; background-color: #EEEEEE;" >
+            <v-toolbar density="compact">
+              <v-toolbar-title class="text-center font-weight-medium ms-10" style="font-size: medium;">{{ email }}</v-toolbar-title>
+              <v-btn icon="mdi-close" dark  @click="dialog = false" density="compact"></v-btn>
+            </v-toolbar>
+            <v-avatar size="100" class="mx-auto">
+              <img src="@/assets/images/avatar.jpg" alt="Avatar" style="object-fit: cover; width: 100%; height: 100%;">
+            </v-avatar>
+            <h2 class="mx-auto font-weight-regular">Hi, {{ name }}!</h2>
+            <v-btn class="mb-4 mx-auto" rounded color="primary" variant="outlined">Manage your Account</v-btn>
+            <v-container>
+              <v-card style="border-radius: 25px;">
+                <v-list>
+                  <v-list-item prepend-avatar="@/assets/images/avatar.jpg" :title="name" :subtitle="email">
+                  </v-list-item>
+                </v-list>
+                <v-divider></v-divider>
+                <v-list :lines="false" nav>
+                  <v-list-item v-for="(item, i) in items" :key="i" :value="item" rounded="xl" @click="handleItemClick(item)">
+                    <template v-slot:prepend>
+                      <v-icon :icon="item.icon"></v-icon>
+                    </template>
+                    <v-list-item-title v-text="item.text" style="font-size: medium;"></v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-container>
+          </v-card>
+        </v-dialog>
+      </v-avatar>
+    </template>
   </v-app-bar>
 </template>
 
@@ -63,6 +58,8 @@ const handleItemClick = (item) => {
     logout();
   }
 }
+
+const dialog = ref(false)
 
 const router = useRouter();
 
@@ -85,23 +82,20 @@ const loadUser = async () => {
 }
 
 const logout = async () => {
-  const token = localStorage.getItem('userToken')
-  if (!token) {
-    localStorage.removeItem('userToken')
-    router.push('/login')
-  }
   try {
+    const token = localStorage.getItem('userToken')
     await axios.post(BASE_URL + '/logout', null, {
       headers: {
         Authorization: `Bearer ${token}`
       },
     })
+    localStorage.removeItem('userToken');
+    name.value = ''
+    email.value = ''
     window.history.pushState(null, '', '/')
     router.push({
       name: 'Login'
     })
-    name.value = ''
-    email.value = ''
   } catch (error) {
     console.log(error)
   }
