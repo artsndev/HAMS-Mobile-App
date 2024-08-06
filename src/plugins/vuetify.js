251040ed0d553acec7e91bdedff02370a23a3ea6
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-08-06 23:14:47 +0800

    coded by artisandev

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,19 +8,28 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
+// Components
+import * as components from 'vuetify/components';
+
+// Labs
+import { VDateInput } from 'vuetify/labs/VDateInput';
+
 // Composables
 import { createVuetify } from 'vuetify'
 
 const customTheme = {
   colors: {
-    // background: '#ECEFF1',
-    primary: '#2196F3',
+    primary: '#1565C0',
     dark: '#212121'
   }
 }
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    ...components,
+    VDateInput
+  },
   theme: {
     defaultTheme: 'customTheme',
     themes: {
