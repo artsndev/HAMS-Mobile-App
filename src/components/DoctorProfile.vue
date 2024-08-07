37364d56c662ff34c55adbf37addf164c7984831
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-08-07 21:36:57 +0800

    coded by artisandev

--- src/components/DoctorProfile.vue
+++ src/components/DoctorProfile.vue
@@ -49,17 +49,46 @@
         </v-list-item-title>
       </v-list-item>
     </v-list>
-    <v-footer class="position-relative ">
-        <v-btn block color="blue-darken-4 sticky-bottom" rounded="lg">Request For Appointment</v-btn>
-      </v-footer>
+    <v-footer class="position-bottom">
+      <v-dialog transition="dialog-bottom-transition" fullscreen>
+        <template v-slot:activator="{ props: activatorProps }">
+          <v-btn block v-bind="activatorProps" color="blue-darken-4 sticky-bottom" rounded="lg">Request For Appointment</v-btn>
+        </template>
+        <template v-slot:default="{ isActive }">
+        <v-card>
+          <v-card-title class="d-flex justify-space-between align-center">
+            Appointment Form
+            <v-btn icon="mdi-close" variant="text" @click="isActive.value = false"></v-btn>
+          </v-card-title>
+          <v-card-text>
+            <div class=" mb-4">Invite collaborators to your network and grow your connections.</div>
+            <v-select v-model="form.appointment_time" density="compact" clearable chips label="Select a schedule" :items="scheduleItem" variant="outlined" ></v-select>
+            <v-select v-model="form.session_of_appointment" color="primary" variant="outlined" density="compact" :items="items" label="Select a Session of appointment" multiple>
+              <template v-slot:selection="{ item, index }">
+                <v-chip v-if="index < 1">
+                  <span>{{ item.title }}</span>
+                </v-chip>
+                <span v-if="index === 1" class="text-grey text-caption align-self-center">
+                  (+{{ form.session_of_appointment.length - 1 }} others)
+                </span>
+              </template>
+            </v-select>
+            <v-textarea v-model="form.purpose_of_appointment" label="Purpose of Appointment. *" density="compact" :counter="300" class="mb-2" rows="3" variant="outlined" persistent-counter color="primary" auto-grow></v-textarea>
+            <v-btn color="primary" class="text-decoration-none">Submit a Request</v-btn>
+          </v-card-text>
+        </v-card>
+      </template>
+    </v-dialog>
+    </v-footer>
 </template>
 
 <script setup>
 import AppBar from './layouts/DoctorProfileAppBar.vue';
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 import axios from 'axios';
 import { BASE_URL } from '@/web';
 import { useRouter, useRoute } from 'vue-router';
+import dayjs from 'dayjs';
 
 const name = ref('')
 const email = ref('')
@@ -72,6 +101,19 @@ const doctorId = ref(null)
 
 const router = useRouter()
 
+const form = reactive({
+  appointment_time: null,
+  purpose_of_appointment: '',
+  session_of_appointment: null,
+})
+const items = ref(['Website', 'Web Application', 'Mobile App', 'Web Design', 'Cloud Hosting', 'API Development']);
+
+const scheduleItem = ref([])
+
+const formatDate = (dateTime) => {
+    return dayjs(dateTime).format('dddd, MMMM D, YYYY hh:mm A');
+};
+
 const loadUser = async () => {
   try {
     const token = localStorage.getItem('userToken')
@@ -80,12 +122,20 @@ const loadUser = async () => {
         Authorization: `Bearer ${token}`
       }
     })
-    name.value = response.data.data.name
-    email.value = response.data.data.email
-    address.value = response.data.data.address
-    birthdate.value = response.data.data.birthdate
-    phone_number.value = response.data.data.phone_number
-    specialization.value = response.data.data.specialization
+    const doctor = response.data.data
+    name.value = doctor.name
+    email.value = doctor.email
+    address.value = doctor.address
+    birthdate.value = doctor.birthdate
+    phone_number.value = doctor.phone_number
+    specialization.value = doctor.specialization
+    if (Array.isArray(doctor.schedule)) {
+      scheduleItem.value = doctor.schedule.flatMap(sch => {
+        const scheduleTimes = Array.isArray(sch.schedule_time) ? sch.schedule_time : sch.schedule_time.split(',')
+        return scheduleTimes.map(time => formatDate(time))
+      });
+    }
+    // console.log(scheduleItem.value);
   } catch (error) {
     if (error.response.status === 401) {
       localStorage.removeItem('userToken');
