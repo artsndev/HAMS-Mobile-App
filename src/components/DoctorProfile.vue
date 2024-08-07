ee83c7e8a99fe6d24e58bdeb59e14056276c2a74
Al-Fhaigar J. Usman <93189177+artsndev@users.noreply.github.com>
2024-08-08 00:26:19 +0800

    Update DoctorProfile.vue

--- src/components/DoctorProfile.vue
+++ src/components/DoctorProfile.vue
@@ -55,30 +55,33 @@
           <v-btn block v-bind="activatorProps" color="blue-darken-4 sticky-bottom" rounded="lg">Request For Appointment</v-btn>
         </template>
         <template v-slot:default="{ isActive }">
-        <v-card>
-          <v-card-title class="d-flex justify-space-between align-center">
-            Appointment Form
-            <v-btn icon="mdi-close" variant="text" @click="isActive.value = false"></v-btn>
-          </v-card-title>
-          <v-card-text>
-            <div class=" mb-4">Invite collaborators to your network and grow your connections.</div>
-            <v-select v-model="form.appointment_time" density="compact" clearable chips label="Select a schedule" :items="scheduleItem" variant="outlined" ></v-select>
-            <v-select v-model="form.session_of_appointment" color="primary" variant="outlined" density="compact" :items="items" label="Select a Session of appointment" multiple>
-              <template v-slot:selection="{ item, index }">
-                <v-chip v-if="index < 1">
-                  <span>{{ item.title }}</span>
-                </v-chip>
-                <span v-if="index === 1" class="text-grey text-caption align-self-center">
-                  (+{{ form.session_of_appointment.length - 1 }} others)
-                </span>
-              </template>
-            </v-select>
-            <v-textarea v-model="form.purpose_of_appointment" label="Purpose of Appointment. *" density="compact" :counter="300" class="mb-2" rows="3" variant="outlined" persistent-counter color="primary" auto-grow></v-textarea>
-            <v-btn color="primary" class="text-decoration-none">Submit a Request</v-btn>
-          </v-card-text>
-        </v-card>
-      </template>
-    </v-dialog>
+          <v-card>
+            <v-card-title class="d-flex justify-space-between align-center">
+              Appointment Form
+              <v-btn icon="mdi-close" variant="text" @click="isActive.value = false"></v-btn>
+            </v-card-title>
+            <v-card-text>
+              <div class=" mb-4">Invite collaborators to your network and grow your connections.</div>
+              <v-form @submit.prevent="createAppointment">
+                <v-text-field v-model="form.doctor_id" class="d-none"  label="Hello"></v-text-field>
+                <v-select class="mb-2" :error-messages="appointment_error" color="primary" v-model="form.appointment_time" density="compact" clearable chips label="Select a schedule" :items="scheduleItem" variant="outlined" ></v-select>
+                <v-select class="mb-2" :error-messages="appointment_session_error" v-model="form.session_of_appointment" color="primary" variant="outlined" density="compact" :items="items" label="Select a Session of appointment" multiple>
+                  <template v-slot:selection="{ item, index }">
+                    <v-chip v-if="index < 1">
+                      <span>{{ item.title }}</span>
+                    </v-chip>
+                    <span v-if="index === 1" class="text-grey text-caption align-self-center">
+                      (+{{ form.session_of_appointment.length - 1 }} others)
+                    </span>
+                  </template>
+                </v-select>
+                <v-textarea :error-messages="appointment_purpose_error" v-model="form.purpose_of_appointment" label="Purpose of Appointment. *" density="compact" :counter="300" class="mb-2" rows="3" variant="outlined" persistent-counter color="primary" auto-grow></v-textarea>
+                <v-btn color="primary" block type="submit" class="text-decoration-none">Submit a Request</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </template>
+      </v-dialog>
     </v-footer>
 </template>
 
@@ -100,13 +103,6 @@ const specialization = ref('')
 const doctorId = ref(null)
 
 const router = useRouter()
-
-const form = reactive({
-  appointment_time: null,
-  purpose_of_appointment: '',
-  session_of_appointment: null,
-})
-const items = ref(['Website', 'Web Application', 'Mobile App', 'Web Design', 'Cloud Hosting', 'API Development']);
 
 const scheduleItem = ref([])
 
@@ -135,7 +131,6 @@ const loadUser = async () => {
         return scheduleTimes.map(time => formatDate(time))
       });
     }
-    // console.log(scheduleItem.value);
   } catch (error) {
     if (error.response.status === 401) {
       localStorage.removeItem('userToken');
@@ -146,6 +141,98 @@ const loadUser = async () => {
         })
       }, 3000)
     }
+  }
+}
+
+const form = reactive({
+  doctor_id: doctorId,
+  appointment_time: null,
+  purpose_of_appointment: '',
+  session_of_appointment: [],
+})
+const items = ref(['Website', 'Web Application', 'Mobile App', 'Web Design', 'Cloud Hosting', 'API Development']);
+
+const formattedDate = (date) => {
+    if (!date) return '';
+    const d = new Date(date);
+
+    // Pad single digits with leading zeros
+    const pad = (num) => num.toString().padStart(2, '0');
+
+    const year = d.getFullYear();
+    const month = pad(d.getMonth() + 1); // Months are zero-based, so add 1
+    const day = pad(d.getDate());
+    const hours = pad(d.getHours());
+    const minutes = pad(d.getMinutes());
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
+
+const appointment_error = ref('')
+const appointment_session_error = ref('')
+const appointment_purpose_error = ref('')
+
+const error = ref('')
+const timer = ref(null)
+
+const createAppointment = async () => {
+  try {
+    const formData = new FormData();
+    formData.append('doctor_id', form.doctor_id);
+    formData.append('appointment_time', formattedDate(form.appointment_time));
+    formData.append('purpose_of_appointment', form.purpose_of_appointment);
+    formData.append('session_of_appointment', form.session_of_appointment.join(','));
+    const token = localStorage.getItem('userToken')
+    const response = await axios.post(BASE_URL + '/appointment/', formData, {
+      headers : {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const clearValidationErrors = () => {
+      appointment_error.value = '';
+      appointment_session_error.value = '';
+      appointment_purpose_error.value = '';
+    }
+
+    const clearErrorValidation = () => {
+      error.value = '';
+    }
+
+    const setValidationError = () => {
+      clearValidationErrors();
+      timer.value = setTimeout(() => {
+        appointment_error.value = response.data.errors.appointment_time;
+        appointment_session_error.value = response.data.errors.purpose_of_appointment;
+        appointment_purpose_error.value = response.data.errors.session_of_appointment;
+      }, 1);
+      setTimeout(() => {
+        clearValidationErrors();
+      }, 10000);
+    }
+
+    const setErrorValidation = () => {
+      clearErrorValidation();
+      setTimeout(() => {
+        error.value = response.data.message;
+      }, 1);
+      setTimeout(() => {
+        clearErrorValidation();
+      }, 10000);
+    }
+    if(response.data.success){
+      loadUser()
+      scheduleItem.value = scheduleItem.value.filter(time => time !== formatDate(form.appointment_time));
+    } else {
+      if (response.data.errors) {
+        setValidationError();
+      } else {
+        setErrorValidation();
+      }
+    }
+    console.log(response)
+  } catch (error) {
+    console.log(error)
   }
 }
 
